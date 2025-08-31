02e7098e43c8b545c2109169a15af9222f99d684
Sarath-12 <54323932+Sarath-12@users.noreply.github.com>
2025-08-31 12:50:23 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mrcloudbook-cicd-bucket"
+    bucket         = "mybucketsarath2102"
     key            = "my-terraform-environment/main"
     region         = "ap-south-1"
     dynamodb_table = "mrcloudbook-dynamo-db-table"
