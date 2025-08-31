77c4735681054a3a0c46b98e65f0e7a374b4f842
Sarath-12 <54323932+Sarath-12@users.noreply.github.com>
2025-08-31 15:21:20 +0530

    Update1 variables.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "ami_id" {
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "staticwebsite"
+  default     = "staticwebsite2102sarath"
 }
 
 variable "acl" {
